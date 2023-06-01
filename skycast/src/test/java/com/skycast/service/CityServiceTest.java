4a6ada8020323722fed45863369a1d48054f5a81
Zane <76492358+ZaneZJ@users.noreply.github.com>
2023-06-02 00:56:21 +0300

    Added tests

--- skycast/src/test/java/com/skycast/service/CityServiceTest.java
+++ skycast/src/test/java/com/skycast/service/CityServiceTest.java
@@ -1,7 +1,7 @@
 package com.skycast.service;
 
 import com.skycast.exception.CityAlreadyExistsException;
-import com.skycast.model.ApiData;
+import com.skycast.exception.CityNotFoundException;
 import com.skycast.model.City;
 import com.skycast.repo.CityRepo;
 import org.junit.jupiter.api.Test;
@@ -10,15 +10,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 class CityServiceTest {
@@ -30,22 +30,22 @@ class CityServiceTest {
     CityService cityService;
 
     @Test
-    void importCityDataThatDoesNotExists() throws IOException {
+    void importCityData_WhenCityDoesntAlreadyExist_ThenShouldReturnCity() throws IOException {
 
         // Arrange - Given
         String json = "[{\"name\":\"Venice\",\"local_names\":{\"lt\":\"Venecija\",\"it\":\"Venezia\",\"fi\":\"Venetsia\",\"en\":\"Venice\",\"ro\":\"Veneţia\",\"hr\":\"Venecija\",\"sl\":\"Benetke\",\"ku\":\"Wênîs\",\"lb\":\"Venedeg\",\"es\":\"Venecia\",\"zh\":\"威尼斯\",\"tt\":\"Венеция\",\"fy\":\"Feneesje\",\"ca\":\"Venècia\",\"bg\":\"Венеция\",\"pt\":\"Veneza\",\"ja\":\"ヴェネツィア\",\"la\":\"Venetiae\",\"cs\":\"Benátky\",\"sk\":\"Benátky\",\"sr\":\"Венеција\",\"ve\":\"Venesia\",\"eo\":\"Venecio\",\"pl\":\"Wenecja\",\"sv\":\"Venedig\",\"uk\":\"Венеція\",\"rm\":\"Vnescha\",\"fa\":\"ونیز\",\"is\":\"Feneyjar\",\"ko\":\"베네치아\",\"tr\":\"Venedik\",\"nl\":\"Venetië\",\"fr\":\"Venise\",\"et\":\"Veneetsia\",\"he\":\"ונציה\",\"vi\":\"Vơ-ni-dơ\",\"el\":\"Βενετία\",\"be\":\"Венецыя\",\"da\":\"Venedig\",\"cy\":\"Fenis\",\"de\":\"Venedig\",\"kn\":\"ವೆನಿಸ್\",\"hu\":\"Velence\",\"ar\":\"البندقية\",\"ru\":\"Венеция\"},\"lat\":45.4371908,\"lon\":12.3345898,\"country\":\"IT\",\"state\":\"Veneto\"}]";
         String ip = "212.189.172.164";
+        City expectedCity = createCity();
         Mockito.when(cityRepo.existsByNameAndCountry("venice","it")).thenReturn(false);
-        Mockito.when(cityRepo.save(createCity())).thenReturn(createCity());
+        Mockito.when(cityRepo.save(expectedCity)).thenReturn(expectedCity);
         // Act - When
         City testCity = cityService.importCityData(json, ip);
         // Assert - Then
-        assertThat(testCity).isEqualTo(createCity());
-
+        assertThat(testCity).isEqualTo(expectedCity);
     }
 
     @Test
-    void importCityDataThatThrowsError() throws IOException {
+    void importCityData_WhenCityAlreadyExists_ShouldThrowCityAlreadyExistsException() {
 
         // Arrange - Given
         String json = "[{\"name\":\"Venice\",\"local_names\":{\"lt\":\"Venecija\",\"it\":\"Venezia\",\"fi\":\"Venetsia\",\"en\":\"Venice\",\"ro\":\"Veneţia\",\"hr\":\"Venecija\",\"sl\":\"Benetke\",\"ku\":\"Wênîs\",\"lb\":\"Venedeg\",\"es\":\"Venecia\",\"zh\":\"威尼斯\",\"tt\":\"Венеция\",\"fy\":\"Feneesje\",\"ca\":\"Venècia\",\"bg\":\"Венеция\",\"pt\":\"Veneza\",\"ja\":\"ヴェネツィア\",\"la\":\"Venetiae\",\"cs\":\"Benátky\",\"sk\":\"Benátky\",\"sr\":\"Венеција\",\"ve\":\"Venesia\",\"eo\":\"Venecio\",\"pl\":\"Wenecja\",\"sv\":\"Venedig\",\"uk\":\"Венеція\",\"rm\":\"Vnescha\",\"fa\":\"ونیز\",\"is\":\"Feneyjar\",\"ko\":\"베네치아\",\"tr\":\"Venedik\",\"nl\":\"Venetië\",\"fr\":\"Venise\",\"et\":\"Veneetsia\",\"he\":\"ונציה\",\"vi\":\"Vơ-ni-dơ\",\"el\":\"Βενετία\",\"be\":\"Венецыя\",\"da\":\"Venedig\",\"cy\":\"Fenis\",\"de\":\"Venedig\",\"kn\":\"ವೆನಿಸ್\",\"hu\":\"Velence\",\"ar\":\"البندقية\",\"ru\":\"Венеция\"},\"lat\":45.4371908,\"lon\":12.3345898,\"country\":\"IT\",\"state\":\"Veneto\"}]";
@@ -53,30 +53,112 @@ class CityServiceTest {
         Mockito.when(cityRepo.existsByNameAndCountry("venice","it")).thenReturn(true);
         // Act - When + Assert - Then
         assertThatThrownBy(() -> cityService.importCityData(json, ip)).isInstanceOf(CityAlreadyExistsException.class);
-
     }
 
     @Test
-    void getCityByNameAndCountry() {
+    public void getCityByNameAndCountry_WhenCityExists_ShouldReturnCity() throws CityNotFoundException {
+        String name = "TestCity";
+        String country = "TestCountry";
+        City expectedCity = createCity();
+
+        when(cityRepo.findByNameAndCountry(name, country)).thenReturn(Optional.of(expectedCity));
+
+        City actualCity = cityService.getCityByNameAndCountry(name, country);
+
+        assertThat(actualCity).isEqualTo(expectedCity);
+        verify(cityRepo).findByNameAndCountry(name, country);
     }
 
     @Test
-    void getCityByName() {
+    public void getCityByNameAndCountry_WhenCityDoesNotExist_ShouldThrowCityNotFoundException() {
+        String name = "TestCity";
+        String country = "TestCountry";
+
+        when(cityRepo.findByNameAndCountry(name, country)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> cityService.getCityByNameAndCountry(name, country))
+                .isInstanceOf(CityNotFoundException.class)
+                .hasMessageContaining("City and country not found");
+        verify(cityRepo).findByNameAndCountry(name, country);
     }
 
     @Test
-    void addCity() {
+    public void getCityByName_WhenCityExists_ShouldReturnCity() throws CityNotFoundException {
+        String name = "TestCity";
+        City expectedCity = createCity();
 
-//        // Arrange - Given
-//        City city = cityService.addCity(createCity());
-//        // Act - When
-//        City testCity = cityService.importCityData("[{\"name\":\"Venice\",\"local_names\":{\"lt\":\"Venecija\",\"it\":\"Venezia\",\"fi\":\"Venetsia\",\"en\":\"Venice\",\"ro\":\"Veneţia\",\"hr\":\"Venecija\",\"sl\":\"Benetke\",\"ku\":\"Wênîs\",\"lb\":\"Venedeg\",\"es\":\"Venecia\",\"zh\":\"威尼斯\",\"tt\":\"Венеция\",\"fy\":\"Feneesje\",\"ca\":\"Venècia\",\"bg\":\"Венеция\",\"pt\":\"Veneza\",\"ja\":\"ヴェネツィア\",\"la\":\"Venetiae\",\"cs\":\"Benátky\",\"sk\":\"Benátky\",\"sr\":\"Венеција\",\"ve\":\"Venesia\",\"eo\":\"Venecio\",\"pl\":\"Wenecja\",\"sv\":\"Venedig\",\"uk\":\"Венеція\",\"rm\":\"Vnescha\",\"fa\":\"ونیز\",\"is\":\"Feneyjar\",\"ko\":\"베네치아\",\"tr\":\"Venedik\",\"nl\":\"Venetië\",\"fr\":\"Venise\",\"et\":\"Veneetsia\",\"he\":\"ונציה\",\"vi\":\"Vơ-ni-dơ\",\"el\":\"Βενετία\",\"be\":\"Венецыя\",\"da\":\"Venedig\",\"cy\":\"Fenis\",\"de\":\"Venedig\",\"kn\":\"ವೆನಿಸ್\",\"hu\":\"Velence\",\"ar\":\"البندقية\",\"ru\":\"Венеция\"},\"lat\":45.4371908,\"lon\":12.3345898,\"country\":\"IT\",\"state\":\"Veneto\"}]", "212.189.172.164");
-//        // Assert - Then
+        when(cityRepo.findByName(name)).thenReturn(Optional.of(expectedCity));
 
+        City actualCity = cityService.getCityByName(name);
+
+        assertThat(actualCity).isEqualTo(expectedCity);
+        verify(cityRepo).findByName(name);
     }
 
     @Test
-    void deleteCity() {
+    public void getCityByName_WhenCityDoesNotExist_ShouldThrowCityNotFoundException() {
+        String name = "TestCity";
+
+        when(cityRepo.findByName(name)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> cityService.getCityByName(name))
+                .isInstanceOf(CityNotFoundException.class)
+                .hasMessageContaining("City not found");
+        verify(cityRepo).findByName(name);
+    }
+
+    @Test
+    public void addCity_WhenCityDoesNotExist_ShouldReturnSavedCity() throws CityAlreadyExistsException {
+        City city = new City();
+        city.setName("TestCity");
+        city.setCountry("TestCountry");
+
+        when(cityRepo.existsByNameAndCountry(city.getName(), city.getCountry())).thenReturn(false);
+        when(cityRepo.save(city)).thenReturn(city);
+
+        City actualCity = cityService.addCity(city);
+
+        assertThat(actualCity).isEqualTo(city);
+        verify(cityRepo).existsByNameAndCountry(city.getName(), city.getCountry());
+        verify(cityRepo).save(city);
+    }
+
+    @Test
+    public void addCity_WhenCityExists_ShouldThrowCityAlreadyExistsException() {
+        City city = new City();
+        city.setName("TestCity");
+        city.setCountry("TestCountry");
+
+        when(cityRepo.existsByNameAndCountry(city.getName(), city.getCountry())).thenReturn(true);
+
+        assertThatThrownBy(() -> cityService.addCity(city))
+                .isInstanceOf(CityAlreadyExistsException.class)
+                .hasMessageContaining("City already exists");
+        verify(cityRepo).existsByNameAndCountry(city.getName(), city.getCountry());
+    }
+
+    @Test
+    public void deleteCity_WhenCityExists_ShouldCallCityRepoDelete() throws CityNotFoundException {
+        String name = "TestCity";
+        String country = "TestCountry";
+        City city = new City();
+
+        when(cityRepo.findByNameAndCountry(name, country)).thenReturn(Optional.of(city));
+
+        cityService.deleteCity(name, country);
+
+        verify(cityRepo).delete(city);
+    }
+
+    @Test
+    public void deleteCity_WhenCityDoesNotExist_ShouldThrowCityNotFoundException() throws CityNotFoundException {
+        String name = "TestCity";
+        String country = "TestCountry";
+
+        when(cityRepo.findByNameAndCountry(name, country)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> cityService.deleteCity(name, country))
+                .isInstanceOf(CityNotFoundException.class);
     }
 
     private City createCity() {
--- skycast/src/test/java/com/skycast/service/ForecastServiceTest.java
+++ skycast/src/test/java/com/skycast/service/ForecastServiceTest.java
@@ -1,20 +1,206 @@
 package com.skycast.service;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skycast.exception.ForecastAlreadyExistsException;
+import com.skycast.exception.ForecastNotFoundException;
+import com.skycast.model.City;
+import com.skycast.model.Forecast;
+import com.skycast.repo.ForecastRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// Autowires necessary dependencies
-@SpringBootTest
-// Connects to the test database
-@AutoConfigureMockMvc
-// Uses application-test.forecast file instead of the regular one
-@TestPropertySource("classpath:application-test.properties")
-// Resets the database after every test, so that they don't clash
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class ForecastServiceTest {
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+public class ForecastServiceTest {
 
+    private ForecastService forecastService;
+
+    @Mock
+    private ForecastRepo forecastRepo;
+
+    @BeforeEach
+    public void setup() {
+        forecastService = new ForecastService(forecastRepo);
+    }
+
+    @Test
+    public void importForecastData_ShouldReturnSavedForecast() throws IOException {
+        String jsonData = "{\"list\":[{\"dt\":1234567890,\"main\":{\"temp\":10.0,\"feels_like\":8.0,\"temp_min\":5.0,\"temp_max\":15.0,\"pressure\":1000,\"sea_level\":1000,\"grnd_level\":1000,\"humidity\":80,\"temp_kf\":0.5},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"clouds\":{\"all\":0},\"wind\":{\"speed\":2.5,\"deg\":180, \"gust\":95},\"visibility\":10000,\"dt_txt\":\"2023-01-01 12:00:00\"}]}";
+        City cityData = new City();
+        Integer id = 0;
+
+        Forecast expectedForecast = new Forecast();
+        expectedForecast.setDt(1234567890L);
+        expectedForecast.setTemp(BigDecimal.valueOf(10.0));
+        expectedForecast.setFeelsLike(BigDecimal.valueOf(8.0));
+        expectedForecast.setTempMin(BigDecimal.valueOf(5.0));
+        expectedForecast.setTempMax(BigDecimal.valueOf(15.0));
+        expectedForecast.setPressure(1000);
+        expectedForecast.setSeaLevel(1000);
+        expectedForecast.setGrndLevel(1000);
+        expectedForecast.setHumidity(80);
+        expectedForecast.setTempKf(BigDecimal.valueOf(0.5));
+        expectedForecast.setWeatherId(id);
+        expectedForecast.setWeatherMain("Clear");
+        expectedForecast.setWeatherDescription("clear sky");
+        expectedForecast.setWeatherIcon("01d");
+        expectedForecast.setCloudsAll(0);
+        expectedForecast.setWindSpeed(BigDecimal.valueOf(2.5));
+        expectedForecast.setWindDeg(180);
+        expectedForecast.setWindGust(BigDecimal.valueOf(95));
+        expectedForecast.setVisibility(10000);
+        expectedForecast.setDtTxt("2023-01-01 12:00:00");
+        expectedForecast.setCity(cityData);
+
+        when(forecastRepo.save(expectedForecast)).thenReturn(expectedForecast);
+
+        Forecast actualForecast = forecastService.importForecastData(jsonData, cityData, id);
+
+        assertThat(actualForecast).isEqualTo(expectedForecast);
+        verify(forecastRepo).save(expectedForecast);
+    }
+
+    @Test
+    public void getForecastByDtTxtAndCity_WhenForecastExists_ShouldReturnForecast() throws ForecastNotFoundException {
+        String dtTxt = "2023-01-01 12:00:00";
+        City city = new City();
+        Forecast expectedForecast = new Forecast();
+
+        when(forecastRepo.findByDtTxtAndCity(dtTxt, city)).thenReturn(Optional.of(expectedForecast));
+
+        Forecast actualForecast = forecastService.getForecastByDtTxtAndCity(dtTxt, city);
+
+        assertThat(actualForecast).isEqualTo(expectedForecast);
+        verify(forecastRepo).findByDtTxtAndCity(dtTxt, city);
+    }
+
+    @Test
+    public void getForecastByDtTxtAndCity_WhenForecastDoesNotExist_ShouldThrowForecastNotFoundException() {
+        String dtTxt = "2023-01-01 12:00:00";
+        City city = new City();
+
+        when(forecastRepo.findByDtTxtAndCity(dtTxt, city)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> forecastService.getForecastByDtTxtAndCity(dtTxt, city))
+                .isInstanceOf(ForecastNotFoundException.class)
+                .hasMessageContaining("Forecast not found");
+        verify(forecastRepo).findByDtTxtAndCity(dtTxt, city);
+    }
+
+    @Test
+    public void getAllForecasts_ShouldReturnAllForecasts() {
+        List<Forecast> forecastList = new ArrayList<>();
+        Forecast forecast1 = new Forecast();
+        Forecast forecast2 = new Forecast();
+        forecastList.add(forecast1);
+        forecastList.add(forecast2);
+
+        when(forecastRepo.findAll()).thenReturn(forecastList);
+
+        List<Forecast> actualForecasts = forecastService.getAllForecasts();
+
+        assertThat(actualForecasts).isEqualTo(forecastList);
+        verify(forecastRepo).findAll();
+    }
+
+    @Test
+    public void addForecast_WhenForecastDoesNotExist_ShouldReturnSavedForecast() throws ForecastAlreadyExistsException {
+        Forecast forecast = new Forecast();
+
+        when(forecastRepo.existsByDtTxtAndCity(forecast.getDtTxt(), forecast.getCity())).thenReturn(false);
+        when(forecastRepo.save(forecast)).thenReturn(forecast);
+
+        Forecast actualForecast = forecastService.addForecast(forecast);
+
+        assertThat(actualForecast).isEqualTo(forecast);
+        verify(forecastRepo).existsByDtTxtAndCity(forecast.getDtTxt(), forecast.getCity());
+        verify(forecastRepo).save(forecast);
+    }
+
+    @Test
+    public void addForecast_WhenForecastExists_ShouldThrowForecastAlreadyExistsException() {
+        Forecast forecast = new Forecast();
+
+        when(forecastRepo.existsByDtTxtAndCity(forecast.getDtTxt(), forecast.getCity())).thenReturn(true);
+
+        assertThatThrownBy(() -> forecastService.addForecast(forecast))
+                .isInstanceOf(ForecastAlreadyExistsException.class)
+                .hasMessageContaining("Forecast already exists");
+        verify(forecastRepo).existsByDtTxtAndCity(forecast.getDtTxt(), forecast.getCity());
+    }
+
+    @Test
+    public void updateForecast_ShouldReturnUpdatedForecast() throws ForecastNotFoundException, JsonProcessingException {
+        String dtTxt = "2023-01-01 12:00:00";
+        String jsonData = "{\"list\":[{\"dt\":1234567890,\"main\":{\"temp\":10.0,\"feels_like\":8.0,\"temp_min\":5.0,\"temp_max\":15.0,\"pressure\":1000,\"sea_level\":1000,\"grnd_level\":1000,\"humidity\":80,\"temp_kf\":0.5},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"clouds\":{\"all\":0},\"wind\":{\"speed\":2.5,\"deg\":180, \"gust\":95, \"gust\":95},\"visibility\":10000}]}";
+        Integer id = 0;
+        City city = new City();
+        Forecast existingForecast = new Forecast();
+
+        existingForecast.setTemp(BigDecimal.valueOf(10.0));
+        existingForecast.setFeelsLike(BigDecimal.valueOf(8.0));
+        existingForecast.setTempMin(BigDecimal.valueOf(5.0));
+        existingForecast.setTempMax(BigDecimal.valueOf(15.0));
+        existingForecast.setPressure(1000);
+        existingForecast.setSeaLevel(1000);
+        existingForecast.setGrndLevel(1000);
+        existingForecast.setHumidity(80);
+        existingForecast.setTempKf(BigDecimal.valueOf(0.5));
+        existingForecast.setWeatherId(id);
+        existingForecast.setWeatherMain("Clear");
+        existingForecast.setWeatherDescription("clear sky");
+        existingForecast.setWeatherIcon("01d");
+        existingForecast.setCloudsAll(0);
+        existingForecast.setWindSpeed(BigDecimal.valueOf(2.5));
+        existingForecast.setWindDeg(180);
+        existingForecast.setWindGust(BigDecimal.valueOf(95));
+        existingForecast.setVisibility(10000);
+
+        when(forecastRepo.findByDtTxtAndCity(dtTxt, city)).thenReturn(Optional.of(existingForecast));
+        when(forecastRepo.save(existingForecast)).thenReturn(existingForecast);
+
+        Forecast actualForecast = forecastService.updateForecast(dtTxt, jsonData, id, city);
+
+        assertThat(actualForecast).isEqualTo(existingForecast);
+        verify(forecastRepo).save(existingForecast);
+    }
+
+    @Test
+    public void deleteForecast_ShouldCallForecastRepoDelete() throws ForecastNotFoundException {
+        String dtTxt = "2023-01-01 12:00:00";
+        City city = new City();
+        Forecast forecast = new Forecast();
+
+        when(forecastRepo.findByDtTxtAndCity(dtTxt, city)).thenReturn(Optional.of(forecast));
+
+        forecastService.deleteForecast(dtTxt, city);
+
+        verify(forecastRepo).delete(forecast);
+    }
+
+    @Test
+    public void deleteForecast_WhenForecastDoesNotExist_ShouldThrowForecastNotFoundException() {
+        String dtTxt = "2023-01-01 12:00:00";
+        City city = new City();
+
+        when(forecastRepo.findByDtTxtAndCity(dtTxt, city)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> forecastService.deleteForecast(dtTxt, city))
+                .isInstanceOf(ForecastNotFoundException.class);
+    }
 }
