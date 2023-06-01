1ed9403d0c3a1dda1f23d8b58afd38aff9c6bbb4
Zane <76492358+ZaneZJ@users.noreply.github.com>
2023-06-02 00:53:14 +0300

    Updated controllers
    - Removed unnecessary controllers
    - For this task I only need one GET method which should be run on the accessing of the page

--- skycast/src/test/java/com/skycast/controller/ApiControllerTest.java
+++ skycast/src/test/java/com/skycast/controller/ApiControllerTest.java
@@ -8,10 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
